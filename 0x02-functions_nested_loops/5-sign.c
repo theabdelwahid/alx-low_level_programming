c701d5783dd7f65f0856897d9c9b804c8f66b7e8
Wahid <modernhill16@gmail.com>
2023-09-13 07:02:16 -0700

    6-abs.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,25 +1,30 @@
 #include "main.h"
 
 /**
- * print_sign - prints the sign of a number
- * @n: the int to check
- * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
- */
+* print_sign - check if the letter is small,
+* Description: unction takes an integer n as input
+* If n is greater than zero, it prints a plus sign (+) and returns 1.
+* If n is equal to zero, it prints the number 0 and returns 0.
+* If n is less than zero, it prints a minus sign (-) and returns -1.
+* @n: is a parameter that represents an integer input
+* Return: Returns either -1, 0 or 1
+*/
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
-	} else if (n == 0)
-}
-		_putchar(48);
+	}
+	else if (n == 0)
+	{
+
+		_putchar('0');
 		return (0);
-	} else if (n < 0)
+	}
+	else
 	{
 		_putchar('-');
-	}
 		return (-1);
+	}
 }
