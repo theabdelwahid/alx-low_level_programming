f60f6a631bbfc843eb8405d78b0e99428fb83171
Wahid <modernhill16@gmail.com>
2023-09-14 09:03:47 -0700

    3-print_remaining_days.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,6 +11,7 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
+	
 	if (a > b && a > c)
 	{
 		largest = a;
@@ -30,4 +31,3 @@ int largest_number(int a, int b, int c)
 	
 	return (largest);
 }
-
